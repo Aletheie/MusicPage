c7fd934a187374b77a5b4c4432cb576e0ce0c05f
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-17 18:51:36 +0200

    Fix Banner Text

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import "../index.css";
 const Banner = () => {
   return (
     <div className="  col-span-12 mx-auto w-11/12 h-2/5 bg-gradient-to-br from-fuchsia-900 via-violet-500 to-indigo-300 mt-12 rounded-3xl">
-      <div className="mr-20 my-6 text-right text-white text-[70px] font-extrabold leading-tight drop-shadow-md">
+      <div className="mr-20 text-right py-5 text-white text-7xl font-extrabold leading-tight drop-shadow-md">
         Upload
         <br /> Your <br /> Album
       </div>
