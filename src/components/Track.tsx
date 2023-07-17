a0bd8d0a7a28cae116ec4a30cd7ee00b60eb9501
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-17 19:24:14 +0200

    Add Tracks

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,5 +1,16 @@
+import heart from "../assets/heart.svg";
+
 const Track = () => {
-  return <div className="bg-black">Track</div>;
+  return (
+    <div className="w-8/12 mt-4 h-16 rounded-xl bg-[#ededed] flex items-center">
+      <div className="bg-[#f4f4f4] w-12 h-12 rounded-lg ml-2"></div>
+      <div className=" text-sm ml-4">
+        <p>Young And Beautiful</p>
+        <p className="text-gray-700 font-medium">Lana Del Rey</p>
+      </div>
+      <img className="ml-24" src={heart} alt="heart" />
+    </div>
+  );
 };
 
 export default Track;
--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,7 +1,11 @@
+import Track from "./Track";
+
 const TrackList = () => {
   return (
-    <div className=" text-right mr-[280px] mt-24">
+    <div className=" text-left ml-[55%] mt-20 font-bold text-3xl h-full ">
       <p className="">Top Tracks</p>
+      <Track />
+      <Track />
     </div>
   );
 };
