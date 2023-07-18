3cbd3de8da0b6cb7793e1d5e99665dc443475c46
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-18 13:58:34 +0200

    Add svg player button hover

--- src/components/BottomPlayer.tsx
+++ src/components/BottomPlayer.tsx
@@ -1,13 +1,61 @@
 import player from "../assets/PlayerButton.svg";
+import playerBlack from "../assets/PlayerButtonBlack.svg";
 import playerR from "../assets/PlayerR.svg";
+import playerRBlack from "../assets/playerRBlack.svg";
 import playerL from "../assets/PlayerL.svg";
+import playerLBlack from "../assets/playerLBlack.svg";
+import { useState } from "react";
 
 const BottomPlayer = () => {
+  const [hover, setHover] = useState({
+    playerLeft: playerL,
+    player: player,
+    playerRight: playerR,
+  });
+
+  const handleMouseOver = (key: string) => {
+    setHover((prevHover) => ({
+      ...prevHover,
+      [key]:
+        key === "player"
+          ? playerBlack
+          : key === "playerLeft"
+          ? playerLBlack
+          : playerRBlack,
+    }));
+  };
+
+  const handleMouseOut = (key: string) => {
+    setHover((prevHover) => ({
+      ...prevHover,
+      [key]:
+        key === "player" ? player : key === "playerLeft" ? playerL : playerR,
+    }));
+  };
+
   return (
-    <div className="w-11/12  mx-auto mt-20 h-16 rounded-xl bg-[#ededed] flex  justify-center items-center gap-3">
-      <img src={playerL} alt="player left" className="h-4" />
-      <img src={player} alt="player" className="h-9" />
-      <img src={playerR} alt="player right" className="h-4" />
+    <div className="w-11/12 mx-auto mt-20 h-16 rounded-xl bg-[#ededed] flex justify-center items-center gap-3">
+      <img
+        onMouseOver={() => handleMouseOver("playerLeft")}
+        onMouseOut={() => handleMouseOut("playerLeft")}
+        src={hover.playerLeft}
+        alt="player left"
+        className="h-4 hover:black"
+      />
+      <img
+        onMouseOver={() => handleMouseOver("player")}
+        onMouseOut={() => handleMouseOut("player")}
+        src={hover.player}
+        alt="player"
+        className="h-9"
+      />
+      <img
+        onMouseOver={() => handleMouseOver("playerRight")}
+        onMouseOut={() => handleMouseOut("playerRight")}
+        src={hover.playerRight}
+        alt="player right"
+        className="h-4"
+      />
     </div>
   );
 };
