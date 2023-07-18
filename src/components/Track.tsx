541cc188d4fe6c14e73e4798d8bd22238fe1cfa2
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-18 15:11:40 +0200

    Give Track component props

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,14 +1,21 @@
 import heart from "../assets/heart.svg";
 
-const Track = () => {
+interface Props {
+  author: string;
+  songName: string;
+}
+
+const Track = ({ songName, author }: Props) => {
   return (
     <div className="w-8/12 mt-4 h-16 rounded-xl bg-[#ededed] flex items-center">
       <div className="bg-[#f4f4f4] w-12 h-12 rounded-lg ml-2"></div>
       <div className=" text-sm ml-4">
-        <p>Young And Beautiful</p>
-        <p className="text-gray-700 font-medium">Lana Del Rey</p>
+        <p>{songName}</p>
+        <p className="text-gray-700 font-medium">{author}</p>
       </div>
-      <img className="ml-24" src={heart} alt="heart" />
+      <div className="ml-auto mr-5">
+        <img src={heart} alt="heart" />
+      </div>
     </div>
   );
 };
--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -4,8 +4,8 @@ const TrackList = () => {
   return (
     <div className=" text-left ml-[60%] mt-16 font-bold text-3xl w-[45%] ">
       <p className="">Top Tracks</p>
-      <Track />
-      <Track />
+      <Track songName="Young and beautiful" author="Lana Del Rey" />
+      <Track songName="Radio" author="Lana Del Rey" />
     </div>
   );
 };
