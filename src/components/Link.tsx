2a3b857cd2d9b1a43ed650cafcc7c5c01447c906
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-18 10:50:48 +0200

    Add more navlinks

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,13 +1,19 @@
 interface Props {
   icon: string;
   text: string;
+  textColor: string;
+  divParams?: string;
 }
 
-const Link = ({ icon, text }: Props) => {
+const Link = ({ icon, text, textColor, divParams }: Props) => {
   return (
-    <div className="flex flex-row items-end mt-5 ms-6 font-semibold">
+    <div
+      className={`flex flex-row items-end mt-9 ms-6 font-semibold ${
+        divParams ? divParams : ""
+      }`}
+    >
       <img src={icon} alt="svgIcon" />
-      <p className="ms-3">{text}</p>
+      <p className={`ms-3 ${textColor}`}>{text}</p>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,47 @@
 import Link from "./Link";
 import home from "../assets/home.svg";
-//import layer from "../assets/Layers_fill.svg";
+import layer from "../assets/Layers_fill.svg";
+import add from "../assets/Chat_plus_fill.svg";
+import headphone from "../assets/Headphones_fill.svg";
+import login from "../assets/Sign_in_squre_fill.svg";
+
+const linksData = [
+  { icon: home, text: "Home", textColor: "text-black" },
+  { icon: layer, text: "Albums", textColor: "text-gray-700" },
+  { icon: add, text: "Add", textColor: "text-gray-700" },
+  { icon: headphone, text: "Lana Del Rey", textColor: "text-gray-700" },
+  { icon: headphone, text: "Lofi", textColor: "text-gray-700" },
+  {
+    icon: login,
+    text: "Login",
+    textColor: "text-black",
+    divParams: "absolute bottom-9",
+  },
+];
 
 const Navbar = () => {
   return (
-    <div className="w-full col-span-2 bg-[#ededed]">
-      <Link icon={home} text="Home" />
+    <div className="w-full col-span-2 bg-[#ededed] ">
+      {linksData.slice(0, 1).map((link, index) => (
+        <Link
+          key={index}
+          icon={link.icon}
+          text={link.text}
+          textColor={link.textColor}
+        />
+      ))}
+
+      <div className="mt-16">
+        {linksData.slice(1).map((link, index) => (
+          <div key={index} className={link.divParams}>
+            <Link
+              icon={link.icon}
+              text={link.text}
+              textColor={link.textColor}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
