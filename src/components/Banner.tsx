99e3db1cdbfdca5cec81922ae37a6ec5160fe718
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-21 13:03:14 +0200

    Make ipod responsive

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,8 +2,8 @@ import "../index.css";
 
 const Banner = () => {
   return (
-    <div className="col-span-12 mx-auto w-11/12 h-2/5 bg-gradient-to-br from-fuchsia-900 via-violet-500 to-indigo-300 mt-10 rounded-3xl drop-shadow-sm flex justify-end items-center flex-wrap ">
-      <h1 className="drop-shadow-md pr-80 lg:pr-20 text-end text-white text-7xl font-extrabold w-1/3 leading-tight">
+    <div className="col-span-12 mx-auto w-11/12 h-2/5 md:h-1/5 lg:h-2/5 bg-gradient-to-br from-fuchsia-900 via-violet-500 to-indigo-300 mt-10 rounded-3xl drop-shadow-sm flex justify-end md:justify-center lg:justify-end items-center flex-wrap md:flex-nowrap lg:flex-wrap">
+      <h1 className="drop-shadow-md pr-80 md:pr-0 lg:pr-20 md:text-center lg:text-right text-white text-7xl md:text-6xl lg:text-7xl font-extrabold w-1/3 md:w-[100%] lg:w-1/3 leading-tight">
         Upload Your Album
       </h1>
     </div>
--- src/components/Ipod.tsx
+++ src/components/Ipod.tsx
@@ -6,7 +6,7 @@ const Ipod = () => {
     <img
       src={ipod}
       alt="ipod"
-      className="hidden md:block w-2/3 md:w-3/5 absolute -left-5 md:left-35 floating"
+      className="hidden md:block w-2/3 md:w-3/5 absolute -left-5 md:left-35 md:bottom-32 lg:bottom-0 lg:top-5 floating md:"
     />
   );
 };
