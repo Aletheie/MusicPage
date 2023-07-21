b91082ecefa744a42adc1136d5dc4252984fdee1
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-21 13:57:42 +0200

    Change heart svg to heart icon and implement handleClick function

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,4 +1,5 @@
-import heart from "../assets/heart.svg";
+import { useState } from "react";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
 interface Props {
   author: string;
@@ -6,6 +7,12 @@ interface Props {
 }
 
 const Track = ({ songName, author }: Props) => {
+  const [isFilledHeart, setIsFilledHeart] = useState(false);
+
+  const handleClick = () => {
+    setIsFilledHeart(!isFilledHeart);
+  };
+
   return (
     <div className="w-4/5 md:w-8/12 mt-6 md:mt-4 h-16 rounded-xl bg-[#ededed] flex items-center">
       <div className="bg-[#f4f4f4] w-12 h-12 rounded-lg ml-2"></div>
@@ -14,7 +21,13 @@ const Track = ({ songName, author }: Props) => {
         <p className="text-gray-700 font-medium">{author}</p>
       </div>
       <div className="ml-auto mr-5">
-        <img src={heart} alt="heart" />
+        <button onClick={handleClick}>
+          {isFilledHeart ? (
+            <AiFillHeart className="text-2xl text-gray-700" />
+          ) : (
+            <AiOutlineHeart className="text-2xl text-gray-700" />
+          )}
+        </button>
       </div>
     </div>
   );
