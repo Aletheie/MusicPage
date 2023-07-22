08690f6a990f16be381e30f16883ff76a2b08dcd
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-22 12:53:15 +0200

    Refactor - narrow input group

--- client/src/components/LoginInputGroup.tsx
+++ client/src/components/LoginInputGroup.tsx
@@ -1,8 +1,12 @@
-import login from "../assets/icons8-login-64.png";
 import "../index.css";
 import TextInput from "./TextInput";
 
-const LoginInputGroup = () => {
+interface Props {
+  icon: string;
+  buttonText: string;
+}
+
+const LoginInputGroup = ({ icon, buttonText }: Props) => {
   const handleFormSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
     console.log("form submitted");
@@ -13,9 +17,9 @@ const LoginInputGroup = () => {
       action=""
       className="flex justify-center w-full h-screen items-center relative"
     >
-      <div className="w-3/4 md:w-1/2 lg:w-1/3 h-2/5 rounded-3xl shadow-xl bg-[#f4f4f4]">
+      <div className="w-3/4 md:w-1/2  h-2/5 rounded-3xl shadow-xl bg-[#f4f4f4]">
         <div className="w-full bg-gradient-to-br from-fuchsia-900 via-violet-500 to-indigo-300 h-1/4  rounded-t-3xl"></div>
-        <img src={login} alt="cd image" className="floating mx-auto -mt-10" />
+        <img src={icon} alt="cd image" className="floating mx-auto -mt-10" />
         <div className="mt-8">
           <TextInput placeholder="Email" type="email" />
           <TextInput placeholder="Password" type="password" />
@@ -24,7 +28,7 @@ const LoginInputGroup = () => {
           onSubmit={handleFormSubmit}
           className="w-3/4 md:w-1/2 lg:w-1/3 bg-[#ededed] hover:bg-[#eaeaea] h-[8%] rounded-b-3xl absolute bottom-60 lg:bottom-56"
         >
-          <span className="font-semibold text-gray-500">Submit</span>
+          <span className="font-semibold text-gray-500">{buttonText}</span>
         </button>
       </div>
     </form>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,11 +1,12 @@
 import LoginInputGroup from "../components/LoginInputGroup";
-import SignUpInputGroup from "../components/SignUpInputGroup";
+import login from "../assets/icons8-login-64.png";
+import signup from "../assets/icons8-access-64.png";
 
 const Login = () => {
   return (
     <div className="w-full h-screen col-span-12 lg:col-span-10 bg-[#f4f4f4] rounded-l-3xl shadow-md overflow-hidden flex justify-center">
-      <LoginInputGroup />
-      <SignUpInputGroup />
+      <LoginInputGroup icon={login} buttonText="Login" />
+      <LoginInputGroup icon={signup} buttonText="SignUp" />
     </div>
   );
 };
