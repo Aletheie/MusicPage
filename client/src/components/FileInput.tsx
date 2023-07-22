a2f2716fb5540a4d224f7d6020515064a481b5c8
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-22 18:45:23 +0200

    Change upload svg to react icon

--- client/src/components/FileInput.tsx
+++ client/src/components/FileInput.tsx
@@ -1,3 +1,5 @@
+import { AiOutlineCloudUpload } from "react-icons/ai";
+
 interface Props {
   setSelectedFile: (file: File | null) => void;
   selectedFile: File | null;
@@ -18,21 +20,7 @@ const FileInput = ({ setSelectedFile, selectedFile }: Props) => {
         className="flex flex-col items-center justify-center w-4/5 h-1/5 border-2 border-gray-300 border-dashed rounded-3xl cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
       >
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg
-            className="w-8 h-8 mb-4 text-violet-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 16"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-            />
-          </svg>
+          <AiOutlineCloudUpload className="w-12 h-12 text-violet-500 hover:text-violet-600 dark:text-gray-400" />
           <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
             <span className="font-semibold">Click to upload</span> or drag and
             drop
