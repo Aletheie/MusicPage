6e4944afe020ae64aa113f8b2c26931e9a2babe8
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-24 10:53:35 +0200

    ONO TO FUNGUJE 🙈!

--- server/routes/songRoutes.ts
+++ server/routes/songRoutes.ts
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv";
 import express from "express";
+import multer from "multer";
 import { Request, Response } from "express";
 
 interface Song {
@@ -15,14 +16,34 @@ dotenv.config();
 
 const router = express.Router();
 
-router.post("/", async (req: Request<{}, {}, Song>, res: Response) => {
-  try {
-    console.log(req.body.songName);
-    res.send(req.body.songName);
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
+
+const upload = multer({ storage: storage });
+
+router.post(
+  "/",
+  upload.single("songFile"),
+  async (req: Request<{}, {}, Song>, res: Response) => {
+    const { songName, songAuthor } = req.body;
+    const songFile = req.file;
+    if (!songName) {
+      return res.send("Please enter song name");
+    }
+    try {
+      console.log(songName, songAuthor, songFile);
+      res.send(req.body);
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  }
+);
 
 export default router;
