04df27c661bf29d2a1ba07a766b4bc7f09ca87cd
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-26 08:46:02 +0200

    Iprove removeSeeds.ts function

--- server/mongoDB/removeSeeds.ts
+++ server/mongoDB/removeSeeds.ts
@@ -1,6 +1,13 @@
-import song from "./models/song";
+import song from "./models/song.js";
+import connectDB from "./connect.js";
 
 const removeAll = async () => {
-  await song.deleteMany({});
+  try {
+    connectDB("mongodb://127.0.0.1:27017/musicPage");
+    await song.deleteMany({});
+    console.log("All songs removed");
+  } catch (error) {
+    console.log(error);
+  }
 };
 removeAll();
