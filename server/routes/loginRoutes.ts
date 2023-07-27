52f375263f9abfd77f73768b2bb36402e0081f24
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-27 12:40:12 +0200

    Implement hashing

--- server/routes/loginRoutes.ts
+++ server/routes/loginRoutes.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import { Request, Response } from "express";
 import LoginType from "../utils/Login";
+import bcrypt from "bcryptjs";
+import User from "../mongoDB/models/user.js";
 
 dotenv.config();
 
@@ -11,10 +13,35 @@ router.route("/").get((req, res) => {
   res.send("Helolo");
 });
 
-router.route("/").post((req: Request<{}, {}, LoginType>, res: Response) => {
-  const { username, password, email } = req.body;
-  console.log(`from backend: ${username} ${password}`);
-  res.send("Helolo");
-});
+router
+  .route("/")
+  .post(async (req: Request<{}, {}, LoginType>, res: Response) => {
+    const { username, password, email } = req.body;
+    if (username) {
+      // Sign up
+      const salt = await bcrypt.genSalt(10);
+      const passwordHash = await bcrypt.hash(password, salt);
+      const newUser = new User({
+        name: username,
+        email,
+        passwordHash,
+      });
+      await newUser.save();
+      res.send("User signed up successfully");
+    } else {
+      // Log in
+      const user = await User.findOne({ email });
+      if (!user) {
+        res.status(400).send("Invalid credentials");
+      } else {
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+        if (isMatch) {
+          res.send("User logged in successfully");
+        } else {
+          res.status(400).send("Invalid credentials");
+        }
+      }
+    }
+  });
 
 export default router;
