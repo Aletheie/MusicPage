adec22ab40dc9e2b5d1c3c1a41b86fd567fa0cb0
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-27 12:41:51 +0200

    Change seed remover

--- server/mongoDB/removeSeeds.ts
+++ server/mongoDB/removeSeeds.ts
@@ -1,10 +1,12 @@
 import Song from "./models/song.js";
+import User from "./models/user.js";
 import connectDB from "./connect.js";
 
 const removeAll = async () => {
   try {
     connectDB("mongodb://127.0.0.1:27017/musicPage");
     await Song.deleteMany({});
+    await User.deleteMany({});
     console.log("All songs removed");
   } catch (error) {
     console.log(error);
