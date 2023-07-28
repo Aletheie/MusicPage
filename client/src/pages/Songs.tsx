64ccb4e74a3bdd360ea611e4722de09184f0144c
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-28 16:55:06 +0200

    Implement songs page

--- client/src/pages/Songs.tsx
+++ client/src/pages/Songs.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Track from "../components/Track";
+import { Link } from "react-router-dom";
 
 interface SongType {
   songName: string;
@@ -31,9 +32,22 @@ const Songs = () => {
     <div className="w-full h-screen col-span-12 lg:col-span-10 bg-[#f4f4f4] rounded-l-3xl shadow-md overflow-hidden">
       <div className="w-2/3 h-screen mx-auto flex justify-center items-center flex-col">
         <h1 className="text-gray-700 font-bold text-5xl pb-5">All Songs</h1>
-        {songList.map((song, idx) => (
-          <Track key={idx} songName={song.songName} author={song.songAuthor} />
-        ))}
+        {songList.length ? (
+          songList.map((song, idx) => (
+            <Track
+              key={idx}
+              songName={song.songName}
+              author={song.songAuthor}
+            />
+          ))
+        ) : (
+          <p className="text-gray-700 font-semibold text-2xl">
+            No Songs, you can add them{" "}
+            <Link to="/songs/add" className=" text-blue-600">
+              here
+            </Link>
+          </p>
+        )}
       </div>
     </div>
   );
