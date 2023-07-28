05cda880926f6057c1ff72be11bd25e9282160f0
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-28 16:48:44 +0200

    Change fonts

--- client/src/components/Track.tsx
+++ client/src/components/Track.tsx
@@ -17,7 +17,7 @@ const Track = ({ songName, author }: Props) => {
     <div className="w-4/5 md:w-8/12 mt-6 md:mt-4 h-16 rounded-xl bg-[#ededed] flex items-center">
       <div className="bg-[#f4f4f4] w-12 h-12 rounded-lg ml-2"></div>
       <div className=" text-sm ml-4">
-        <p>{songName}</p>
+        <p className="text-black font-semibold">{songName}</p>
         <p className="text-gray-700 font-medium">{author}</p>
       </div>
       <div className="ml-auto mr-5">
