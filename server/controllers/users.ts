7243d59fa8022abf6e1628001aa1e1d9e5ea3a81
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-28 11:14:06 +0200

    refactor: move createUser function to its controller

---
+++ server/controllers/users.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import UserType from "../utils/Login";
+import bcrypt from "bcryptjs";
+import User from "../mongoDB/models/user.js";
+import { Session } from "../utils/authMiddleware.js";
+
+const createUser = async (
+  req: Request<{}, {}, UserType> & { session: Session },
+  res: Response
+) => {
+  const { username, password, email } = req.body;
+
+  if (username) {
+    // Sign up
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      name: username,
+      email,
+      passwordHash,
+    });
+    await newUser.save();
+    req.session.authenticated = true;
+    req.session.email = email;
+    res.send("User signed up successfully");
+  } else {
+    // Log in
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(400).send("Invalid credentials");
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.passwordHash);
+
+    if (isMatch) {
+      req.session.authenticated = true;
+      req.session.email = email;
+      res.send("User logged in successfully");
+    } else {
+      res.status(400).send("Invalid credentials");
+    }
+  }
+};
+
+export { createUser };
--- server/routes/loginRoutes.ts
+++ server/routes/loginRoutes.ts
@@ -1,9 +1,5 @@
 import express from "express";
-import { Request, Response } from "express";
-import UserType from "../utils/Login";
-import bcrypt from "bcryptjs";
-import User from "../mongoDB/models/user.js";
-import { Session } from "../utils/authMiddleware.js";
+import { createUser } from "../controllers/users.js";
 
 const router = express.Router();
 
@@ -11,47 +7,6 @@ router.get("/", (req, res) => {
   res.send("Helolo");
 });
 
-router.post(
-  "/",
-  async (
-    req: Request<{}, {}, UserType> & { session: Session },
-    res: Response
-  ) => {
-    const { username, password, email } = req.body;
-
-    if (username) {
-      // Sign up
-      const salt = await bcrypt.genSalt(10);
-      const passwordHash = await bcrypt.hash(password, salt);
-      const newUser = new User({
-        name: username,
-        email,
-        passwordHash,
-      });
-      await newUser.save();
-      req.session.authenticated = true;
-      req.session.email = email;
-      res.send("User signed up successfully");
-    } else {
-      // Log in
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        res.status(400).send("Invalid credentials");
-        return;
-      }
-
-      const isMatch = await bcrypt.compare(password, user.passwordHash);
-
-      if (isMatch) {
-        req.session.authenticated = true;
-        req.session.email = email; //tady to funguje
-        res.send("User logged in successfully");
-      } else {
-        res.status(400).send("Invalid credentials");
-      }
-    }
-  }
-);
+router.post("/", createUser);
 
 export default router;
