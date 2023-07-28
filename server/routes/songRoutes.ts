62a0f637b9c100799369837a855c9424099b3830
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-28 15:34:49 +0200

    Send songs from database to frontend

--- server/routes/songRoutes.ts
+++ server/routes/songRoutes.ts
@@ -1,8 +1,13 @@
 import * as dotenv from "dotenv";
 import express from "express";
+import { Request, Response } from "express";
+import UserType from "../utils/Login";
+import { Session } from "../utils/authMiddleware.js";
 import { upload } from "../cloudinary/index.js";
 import { createSong } from "../controllers/songs.js";
 import { requireAuth } from "../utils/authMiddleware.js";
+import Song from "../mongoDB/models/song.js";
+import User from "../mongoDB/models/user.js";
 
 dotenv.config();
 
@@ -11,5 +16,24 @@ const router = express.Router();
 const uploadSingle = upload().single("songFile");
 
 router.post("/api/songs", requireAuth, uploadSingle, createSong);
+
+router.post(
+  "/songs",
+  async (
+    req: Request<{}, {}, UserType> & { session: Session },
+    res: Response
+  ) => {
+    try {
+      const userFromDatabase = await User.findOne({ email: req.session.email });
+      const songsFromDatabase = await Song.find({
+        user: userFromDatabase?._id,
+      });
+      res.send(songsFromDatabase);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 
 export default router;
