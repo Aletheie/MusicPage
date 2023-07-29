5f49ce0d79edbe17d523db1b601008cfa6ce4d47
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-29 13:19:10 +0200

    Fix DOM errors

--- client/src/components/TextInput.tsx
+++ client/src/components/TextInput.tsx
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 interface Props {
   placeholder: string;
   type: string;
@@ -10,12 +12,13 @@ const TextInput = ({ placeholder, type, setInputText, inputText }: Props) => {
     <div className="w-4/5 mx-auto mt-4">
       <input
         type={type}
-        id="first_name"
+        id={uuidv4()}
         className="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder={placeholder}
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
         required
+        autoComplete="off"
       />
     </div>
   );
