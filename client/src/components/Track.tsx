738a84962035ef31143b595a59fabe0c3abeca05
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-29 11:26:12 +0200

    Implement function for playing your own music

--- client/src/components/Track.tsx
+++ client/src/components/Track.tsx
@@ -1,28 +1,46 @@
 import { useState } from "react";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 import { SongType } from "../utils/SongType";
-//import useSound from "use-sound";
+import useSound from "use-sound";
 
 interface Props {
   song: SongType;
 }
 
 const Track = ({ song }: Props) => {
-  // const [currentSound, setCurrentSound] = useState<string>("");
+  const [currentSound, setCurrentSound] = useState<string>("");
+  const [isPlaying, setIsPlaying] = useState(false);
   const [isFilledHeart, setIsFilledHeart] = useState(false);
-  //const [play, { pause }] = useSound(currentSound);
-
-  // const handleHeartTrackClick = (path: string) => {
-  //   setCurrentSound(path);
-  // };
+  const [play, { pause }] = useSound(currentSound);
 
   const handleHeartClick = () => {
     setIsFilledHeart(!isFilledHeart);
   };
 
   return (
-    <div className="w-4/5 md:w-8/12 mt-6 md:mt-4 h-16 rounded-xl bg-[#ededed] hover:bg-[#e7e7e7] flex items-center">
-      <div className="bg-[#f4f4f4] w-12 h-12 rounded-lg ml-2"></div>
+    <div className=" cursor-pointer w-4/5 md:w-8/12 mt-6 md:mt-4 h-16 rounded-xl bg-[#ededed] hover:bg-[#e7e7e7] flex items-center">
+      <div className="bg-[#f4f4f4] w-12 h-12 rounded-lg ml-2 flex justify-center items-center">
+        {isPlaying ? (
+          <BsFillPauseFill
+            className="text-4xl text-[#ededed]"
+            onClick={() => {
+              setCurrentSound(song.songFile.path);
+              pause();
+              setIsPlaying(false);
+            }}
+          />
+        ) : (
+          <BsFillPlayFill
+            className="text-4xl text-[#ededed]"
+            onClick={() => {
+              setCurrentSound(song.songFile.path);
+              play();
+              setIsPlaying(true);
+            }}
+          />
+        )}
+      </div>
       <div className=" text-sm ml-4">
         <p className="text-black font-semibold">{song.songName}</p>
         <p className="text-gray-700 font-medium">{song.songAuthor}</p>
