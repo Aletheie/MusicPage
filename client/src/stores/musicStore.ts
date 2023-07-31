8b7ed39f403d023e5724413e32e08e0af0ccb65f
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-31 14:13:25 +0200

    Improve zustand music store

--- client/src/stores/musicStore.ts
+++ client/src/stores/musicStore.ts
@@ -2,12 +2,16 @@ import { create } from "zustand";
 
 interface Store {
   isPlaying: boolean;
+  songName?: string;
   setIsPlaying: () => void;
+  setSongName: (songName: string) => void;
 }
 
 const useMusicStore = create<Store>((set) => ({
   isPlaying: false,
+  songName: undefined,
   setIsPlaying: () => set((state) => ({ isPlaying: !state.isPlaying })),
+  setSongName: (songName) => set(() => ({ songName })),
 }));
 
 export default useMusicStore;
