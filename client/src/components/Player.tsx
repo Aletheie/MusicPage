f1bc5067847a7508062d223767a8773b28ddf8b4
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-07-31 14:53:26 +0200

    Display the title and author of the song being played in the navigation bar

--- client/src/components/Player.tsx
+++ client/src/components/Player.tsx
@@ -1,9 +1,16 @@
+import useMusicStore from "../stores/musicStore";
+
 const Player = () => {
+  const { song } = useMusicStore((s) => ({ song: s.song }));
   return (
     <div className="text-sm flex flex-col items-center mt-9">
       <div className="w-44 h-44 bg-[#f4f4f4] rounded-2xl"></div>
-      <p className="font-bold mt-3">Say Yes To Heaven</p>
-      <p className="text-gray-700 font-medium">Lana Del Rey</p>
+      <p className="font-bold mt-3">
+        {song.songName.length ? song.songName : "No Song is playing"}
+      </p>
+      <p className="text-gray-700 font-medium">
+        {song.songAuthor.length ? song.songAuthor : " - "}
+      </p>
     </div>
   );
 };
