62d8c15c4053ebef72138b16b7ced5826f376fb4
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-08-01 11:03:42 +0200

    Add music loader to Player component

--- client/src/components/Player.tsx
+++ client/src/components/Player.tsx
@@ -1,10 +1,16 @@
+import { ScaleLoader } from "react-spinners";
 import useMusicStore from "../stores/musicStore";
 
 const Player = () => {
   const { song } = useMusicStore((s) => ({ song: s.song }));
+  const { isGlobalPlaying } = useMusicStore((s) => ({
+    isGlobalPlaying: s.isGlobalPlaying,
+  }));
   return (
     <div className="text-sm flex flex-col items-center mt-9">
-      <div className="w-44 h-44 bg-[#f4f4f4] rounded-2xl"></div>
+      <div className="w-44 h-44 bg-[#f4f4f4] rounded-2xl flex justify-center items-center">
+        {isGlobalPlaying && <ScaleLoader color="#4b5563" />}
+      </div>
       <p className="font-bold mt-3">
         {song.songName.length ? song.songName : "No Song is playing"}
       </p>
