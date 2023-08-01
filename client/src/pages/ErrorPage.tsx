9be7a9589cf34dfe21e1bc949e17277532e2d445
Anicka J 3 <anna.jungmannova@ssps.cz>
2023-08-01 14:13:11 +0200

    Design error page

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,29 +1,24 @@
 import service from "../assets/icons8-services-256.png";
+import "../index.css";
 
 const ErrorPage = () => {
   return (
-    <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
-      <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
-        <div className="relative">
-          <div className="absolute">
-            <div>
-              <h1 className="my-2 text-gray-800 font-bold text-2xl">
-                Looks like you've found the doorway to the great nothing
-              </h1>
-              <p className="my-2 text-gray-800">
-                Sorry about that! Please visit our hompage to get where you need
-                to go.
-              </p>
-              <button className="sm:w-full lg:w-auto my-2 border rounded-3xl md py-3 px-6 text-center bg-violet-500 text-white hover:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
-                Take me there!
-              </button>
-            </div>
-          </div>
+    <div className="w-full h-screen flex items-center justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+      <div className="xl:pt-24 w-full xl:w-1/2 pb-12 lg:pb-0">
+        <div className="mx-12 md:mx-10 lg:mx-16">
+          <h1 className=" text-gray-800 font-bold text-2xl">
+            Looks like you've found the doorway to the great nothing
+          </h1>
+          <p className="text-gray-800 ">
+            Sorry about that! Please visit our hompage to get where you need to
+            go.
+          </p>
+          <button className="sm:w-full lg:w-auto my-2 border rounded-3xl mt-3 py-3 px-6 text-center bg-violet-500 text-white hover:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
+            Take me there!
+          </button>
         </div>
       </div>
-      <div>
-        <img src={service} className=" xs" />
-      </div>
+      <img src={service} className="floating" />
     </div>
   );
 };
